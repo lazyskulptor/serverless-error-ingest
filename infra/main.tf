a02197a38add94281e3855d7d7be32d730fcaa00
Hyeonjun Park <lazyskulptor@gmail.com>
2026-09-08 20:01:10 +0900

    fix: authorize full API Gateway route paths

--- infra/main.tf
+++ infra/main.tf
@@ -266,7 +266,7 @@ resource "aws_lambda_permission" "api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.main[each.value.lambda].function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/${each.value.http}/${each.key}"
+  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/${each.value.http}/api/*/${each.key}"
 }
 
 # ---------------------------------------------------------------------------
